bc4b6eb2cc923ca17e5860836343c3a6d523bad7
Chanon Trising <holyddog@gmail.com>
2017-07-14 18:11:51 +0700

    initial project

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'shop-manager',
+    templateUrl: 'app.component.html'
+})
+export class AppComponent {
+    title = 'Shop Manager';
+}
